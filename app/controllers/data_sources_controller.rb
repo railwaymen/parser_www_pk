19e5eb9a5b5e7aaa51c97a963b746f3f27ebb498
ktopolski <karoltopolski92@gmail.com>
2015-03-09 09:14:08 +0100

    Rails 4.2.0 simple API: based on respond_to since respond_with is removed.

--- app/controllers/data_sources_controller.rb
+++ app/controllers/data_sources_controller.rb
@@ -1,8 +1,10 @@
 class DataSourcesController < ApplicationController
-  respond_to :html, :xml, :json
-
   def index
     @data_sources = DataSource.all
-    respond_with @data_sources
+    respond_to do |format|
+      format.html
+      format.xml  { render xml: @data_sources }
+      format.json { render json: @data_sources }
+    end
   end
 end
